bb26c3ff118030b50a6de4174e99b265e2d14627
Horacio-Sutara <rhsuty@gmail.com>
2025-11-17 19:35:56 -0300

    ruta de donde esta el repositorio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Hotel-Front/',
+  base: '/Hotel-Front-/',
   plugins: [
     react(),    
     tailwindcss()
